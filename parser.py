fe3aa34d4bdcbf7db2042eb5ea61801265b29691
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2017-09-21 14:54:51 -0400

    parsing title

--- parser.py
+++ parser.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python3
 import urllib.request
 import re
-
+import json
 
 def generURL(a):
-	url = 'http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID='+str(a)
-	return url
+    url = 'http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID='+str(a)
+    return url
 
 
 
 for i in range(1,3):
-	url = generURL(i)
-	req = urllib.request.Request(url)
-	resp = urllib.request.urlopen(req)
-	respData = resp.read()
-	paragraphs = re.findall(r'<!-- START Spell -->(.*?)<!-- END Spell -->',str(respData))
-	print(str(paragraphs))
-	print()
+    url = generURL(i)
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+    paragraphs = re.findall(r'<!-- START Spell -->(.*?)<!-- END Spell -->',str(respData))
+    print(str(paragraphs))
+    print()
+    Title=re.findall(r'<P>(.*?)<\/p>',str(paragraphs))[0]
+    print(Title)
+    print()
+
