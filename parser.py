4a088a4e796d5e558b65d64e1e78087235d3eb0d
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2017-09-27 20:29:05 -0400

    parser finished

--- parser.py
+++ parser.py
@@ -1,23 +1,62 @@
 #!/usr/bin/python3
-import urllib.request
-import re
 import json
+from lxml import html
+import requests
 
 def generURL(a):
     url = 'http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID='+str(a)
     return url
 
 
+data=[]
+for i in range(1,1972):
+    if(i!=1841):
+        print(i)
+        url = generURL(i)
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        name=tree.xpath("/html/body/div[2]/div[2]/div[1]/p/text()")
+        #print(name)
+        levels=tree.xpath("/html/body/div[2]/div[2]/p[1]/text()[2]")
+        levels=levels[0]
+        levelList=(levels.split(","))
+        level=10000000
+        isWizard=False
+        for i in levelList :
+            #print(i)
+            if 'wizard' in i:
+                #print("#################### SUCCESS ##############")
+                level=int(i.split(' ')[-1])
+                isWizard=True
+            #else:
+                #print("#################### FAIL ##############")
+        if level==10000000:
+            level=int(levelList[0].split(' ')[-1])
 
-for i in range(1,3):
-    url = generURL(i)
-    req = urllib.request.Request(url)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
-    paragraphs = re.findall(r'<!-- START Spell -->(.*?)<!-- END Spell -->',str(respData))
-    print(str(paragraphs))
-    print()
-    Title=re.findall(r'<P>(.*?)<\/p>',str(paragraphs))[0]
-    print(Title)
-    print()
+
+        components=tree.xpath("/html/body/div[2]/div[2]/p[3]/text()")
+        components=components[0].split(',')
+
+        #print(components)
+        spell_resistance=tree.xpath("/html/body/div[2]/div[2]/p[7]/text()[2]")
+        if spell_resistance=='no':
+            spell_resistance=False
+        else:
+            spell_resistance=True
+        #print(spell_resistance)
+        description=tree.xpath("/html/body/div[2]/div[2]/div[5]/p/text()")
+        #print(description)
+        data.append({
+            "name":name[0],
+            "level":level,
+            "isWizard":isWizard,
+            "components":components,
+            "spell_resistance":spell_resistance,
+            "description":description
+        
+        })
+print()
+print(data)
+with open('data.json', 'w') as outfile:  
+    json.dump(data, outfile)
 
