5d4261e806391bdc2f3c9e6dd760624d6e0d9f61
morgane <mcabrol21@gmail.com>
2017-10-13 14:34:50 -0400

    selction de sort

--- mapReduceProjet.js
+++ mapReduceProjet.js
@@ -18,25 +18,47 @@ MongoClient.connect('mongodb://localhost:27017/DandDspells', function(err, db) {
    	var name = this.name;
    	var sortUtilisable = true;
 
-   	if(isWizard == true  && level<4 && (components.indexOf(" V") != -1)){
-   		sortUtilisable = "oui";
+   	if(isWizard == true  && level<=4 && (components.indexOf(" V") != -1) && components.length == 1){
+   		sortUtilisable = true;
    	}else{
-		sortUtilisable = "non";
+   		sortUtilisable = false;
    	}
    	var obj= {level:level, isWizard:isWizard,components:components,sortUtilisable:sortUtilisable};
-	emit(this.name,obj);
+   	emit(this.name,obj);
 
    };
 
 
    var reduce = function (key, values)
    {
-   		emit(key,values);
+   	emit(key,values);
+   	
    };
 
-   collection.mapReduce(map, reduce, {out: {replace: "spells2"}})
+   collection.mapReduce(map, reduce, {out: {replace: "spells2"}}).then(function (collection)
+   {
+   	collection.find().toArray()
+   	.then(function (docs)
+   	{
+   		for(var i=0;i<docs.length;i++){
+   			  console.log(docs[i]);
+   				/*var boolSortUtilisable = docs[i].value.sortUtilisable;
+   				console.log(boolSortUtilisable);
+   				
+   				if(boolSortUtilisable == true){
+   					console.log(docs[i]._id);
+   				}*/
 
-   db.close();
-   console.log("fin");
+   			
+   		}
+   		//console.log(docs);
+
+
+   		db.close();
+   		console.log("fin");
+   	})
+   })
+
+
 
 })
