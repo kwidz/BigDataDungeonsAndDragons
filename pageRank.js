ffa3831cbaa0e7e37ff0002879b5644f5391defb
morgane <mcabrol21@gmail.com>
2017-10-15 21:59:03 -0400

    pageRank fini

--- pageRank.js
+++ pageRank.js
@@ -3,19 +3,19 @@
 */
 
 var MongoClient = require('mongodb').MongoClient(),
-   test = require('assert');
-MongoClient.connect('mongodb://localhost:27017/DandDspells', function(err, db) {
+test = require('assert');
+MongoClient.connect('mongodb://localhost:27017/BddPageRank', function(err, db) {
 
    // Create a test collection
    var collection = db.collection('pageRank');
    //Pour garder le chemin, enregistrer un backpointer.
    var graph =
-       [
-           { _id:"PageA" ,  value:{ backpointer: "", changed: false, type: "full", poid: 0.85, pointeVers: ["PageB", "PageC"], estPointePar: ["PageC"]}},
-           { _id:"PageB", value:{ backpointer: "",changed: false,type: "full", poid: Infinity, pointeVers: ["PageC"], estPointePar: ["PageA"]}},
-           {_id:"PageC",value:{ backpointer: "",changed: false,type: "full", poid: Infinity, pointeVers: ["PageA"], estPointePar: ["PageA","PageB"]}},
-           { _id:"PageD", value:{ backpointer: "",changed: false,type: "full", poid: Infinity, pointeVers: ["PageC"], estPointePar: []}}
-       ];
+   [
+   { _id:"PageA" ,  value:{ pageRank:1, pointeVers: ["PageB", "PageC"], estPointePar: ["PageC"]}},
+   { _id:"PageB", value:{ pageRank:1, pointeVers: ["PageC"], estPointePar: ["PageA"]}},
+   {_id:"PageC",value:{ pageRank:1, pointeVers: ["PageA"], estPointePar: ["PageA","PageB","PageD"]}},
+   { _id:"PageD", value:{ pageRank:1, pointeVers: ["PageC"], estPointePar: []}}
+   ];
 
    // console.log(graph);
 
@@ -33,110 +33,62 @@ MongoClient.connect('mongodb://localhost:27017/DandDspells', function(err, db) {
            var name = this._id;
            var pointeVers = this.value.pointeVers;
            var estPointePar = this.value.estPointePar;
-           var poid = this.value.poid;
+           var pageRank = this.value.pageRank;
 
-           // print("Full objet emit");
-           //print(vertex, this);
-           emit(name, this.value); //Re-émettre la même chose
+          emit(name, 0); //Re-émettre la même chose
 
-           if (poid== Infinity) return; //Si la distance est infinie, pas besoin d'emitter des trucs...
 
            for (var i = 0; i < pointeVers.length; i++) {
 
                var pagePointe = pointeVers[i]; //valere, julien etc
                //  print(adj, distance+1);
-               var objet = {type:"compact", poid:poid+1, backpointer:name};
+               var pageRankModifie = pageRank/pointeVers.length;
 
                //   print("map ", adj, tojson(objet));
-               emit(pagePointe, objet);
-           }
+               emit(pagePointe, pageRankModifie);
+             }
 
-       };
+             emit(name,pointeVers);
+
+           };
 
        //Find lowest distance and write it
        var reduce = function (key, values)
        {
-           print("1er Reduce : ", tojson(key), tojson(values));
-           var full = {};
 
-           //First, find the original one
-           for (var i = 0; i < values.length; i++)
-           {
-               var val = values[i];
-               if (val.type == "full") {
-                   full = val;
-               }
-           }
+        var pointeVers = [];
+        var pageRank = 0;
 
-           //On met full a "aucun changement"
-           full.changed = false;
+        for(var i=0;i<values.length;i++){
+          if (values[i] instanceof Array ){
+            pointeVers =values[i];
+          }else{
+            pageRank += values[i];
+          }
 
-           //Then improve on it
-           for (var i = 0; i < values.length; i++)
-           {
-               var val = values[i];
-               if (val.type == "compact") {
-                   if (val.poid < full.poid) {
-                       full.changed = true;
-                       full.poid = val.poid;
-                       full.backpointer = val.backpointer;
-                   }
-               }
-           }
 
-           print("Full object de ", key);
-           print(tojson(full));
-           return full;
-       };
+        }
 
-       //tf = test function
-       function bfs_iteration(i, max, cb)
-       {
-           // Peform the map reduce
-           collection.mapReduce(map, reduce, {out: {replace: "pageRank"}})
-           .then(function (collection)
-           {
-               collection.find().toArray()
-                   .then(function (docs)
-                   {
-                       console.log(docs);
-                       console.log("************************************************************************");
-                       console.log("************************************************************************");
-                       console.log("************************************************************************");
+        pageRank = 1 - 0.85 + ( 0.85 * pageRank);
+        return {name:key,pageRank:pageRank,pointeVers:pointeVers};
 
-                       //1ere condition d'arrêt. Le graphe converge (algo fini)
-                       //Sinon, on a egalement un maximum d'iterations
-                       //Nombre max d'iterations atteint
-                       console.log("VALEUR DE I = ", i);
-                       if (i == max)
-                           cb();
 
-                       // Peform a simple find and return all the documents
-                       collection.find({"value.changed": true}).toArray()
-                           .then(function (docs)
-                           {
+      }
 
-                               console.log("GROS LOG DES DOCS TROUVES");
-                               console.log("************************************************************************");
-                               console.log(docs);
+      collection.mapReduce(map, reduce, {out: {replace: "pageR"}}).then(function (collection)
+      {
+        collection.find().toArray()
+        .then(function (docs)
+        {
+         
+      console.log(docs);
 
-                               if (docs.length == 0) {
-                                   console.log("SORTIE TAKEN");
-                                   cb();
-                               }
-                               else {
-                                   console.log("ENCORE UNE ITERATION");
-                                   bfs_iteration(i + 1, max, cb);
-                               }
-                           })
-                   })
-           })
-       }
 
-   bfs_iteration(0, 10, function fin() {
-       console.log("Fin du programme!!!");
-       db.close();
-   })
+      db.close();
+      console.log("fin");
+    });
+      });
 
-})
-}) 
+});
+
+    });
